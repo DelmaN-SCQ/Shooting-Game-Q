cde55bc29f4620b42a63269c55c6972d612d1ab4
DelmaN-SCQ <kluskaszymon23@gmail.com>
2025-12-04 08:58:27 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,103 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Player
+let player = {
+    x: canvas.width / 2 - 20,
+    y: canvas.height - 60,
+    width: 40,
+    height: 40,
+    speed: 5
+};
+
+let bullets = [];
+let enemies = [];
+let keys = {};
+
+document.addEventListener("keydown", e => keys[e.key] = true);
+document.addEventListener("keyup",   e => keys[e.key] = false);
+
+// Shoot bullet
+function shoot() {
+    bullets.push({
+        x: player.x + player.width / 2 - 3,
+        y: player.y,
+        width: 6,
+        height: 10,
+        speed: 7
+    });
+}
+
+// Create enemies
+setInterval(() => {
+    enemies.push({
+        x: Math.random() * (canvas.width - 40),
+        y: -50,
+        width: 40,
+        height: 40,
+        speed: 2
+    });
+}, 800);
+
+// Update game loop
+function update() {
+    // Player movement
+    if (keys["ArrowLeft"] && player.x > 0) player.x -= player.speed;
+    if (keys["ArrowRight"] && player.x < canvas.width - player.width) player.x += player.speed;
+    if (keys[" "] ) {
+        if (!player.shooting) {
+            shoot();
+            player.shooting = true;
+        }
+    } else {
+        player.shooting = false;
+    }
+
+    // Move bullets
+    bullets.forEach(b => b.y -= b.speed);
+    bullets = bullets.filter(b => b.y > -20);
+
+    // Move enemies
+    enemies.forEach(e => e.y += e.speed);
+
+    // Collision detection
+    enemies.forEach((e, ei) => {
+        bullets.forEach((b, bi) => {
+            if (b.x < e.x + e.width &&
+                b.x + b.width > e.x &&
+                b.y < e.y + e.height &&
+                b.y + b.height > e.y) {
+                enemies.splice(ei, 1);
+                bullets.splice(bi, 1);
+            }
+        });
+    });
+
+    enemies = enemies.filter(e => e.y < canvas.height + 50);
+}
+
+// Draw everything
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Player
+    ctx.fillStyle = "cyan";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    // Bullets
+    ctx.fillStyle = "yellow";
+    bullets.forEach(b => ctx.fillRect(b.x, b.y, b.width, b.height));
+
+    // Enemies
+    ctx.fillStyle = "red";
+    enemies.forEach(e => ctx.fillRect(e.x, e.y, e.width, e.height));
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
